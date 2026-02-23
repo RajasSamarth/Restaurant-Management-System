0261215c916a9a544aa29e513b62202a514f2558
RajasSamarth <rajassamarth15@gmail.com>
2026-02-23 19:01:39 +0530

    Uploading project

--- src/test/java/com/revision/RestaurantManagementSystem/restaurantManagementSystemTests.java
+++ src/test/java/com/revision/RestaurantManagementSystem/restaurantManagementSystemTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringJpArevisionApplicationTests {
+class restaurantManagementSystemTests {
 
 	@Test
 	void contextLoads() {
